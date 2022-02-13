8fe0148573d8b64a401e115543ee1b88495fea63
itsubaki <1759459+itsubaki@users.noreply.github.com>
2022-02-14 00:20:20 +0900

    Add TestMoreExceptions

--- 30days_of_code_test.go
+++ 30days_of_code_test.go
@@ -585,3 +585,18 @@ func TestExceptions(t *testing.T) {
 	// 		System.out.println("Bad String");
 	// }
 }
+
+func TestMoreExceptions(t *testing.T) {
+	// Not provided for Go
+	//
+	// Java8
+	// class Calculator{
+	// 	public int power(int n, int p) throws Exception{
+	// 		if(n < 0 || p < 0){
+	// 			throw new Exception("n and p should be non-negative");
+	// 		}
+	//
+	// 		return (int)Math.pow(n,p);
+	// 	}
+	// }
+}
