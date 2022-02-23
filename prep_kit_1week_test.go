e1388ff993e1f158a1af9217e3a71a11858e1c92
itsubaki <1759459+itsubaki@users.noreply.github.com>
2022-02-23 15:38:37 +0900

    Add TestMergeTwoSortedLinkedLists

--- prep_kit_1week_test.go
+++ prep_kit_1week_test.go
@@ -544,8 +544,26 @@ func TestMinimumBribes(t *testing.T) {
 	}
 }
 
-func TestMergeLists(t *testing.T) {
+func TestMergeTwoSortedLinkedLists(t *testing.T) {
 	// Not provided for Go
+	//
+	// Java8
+	// static SinglyLinkedListNode mergeLists(SinglyLinkedListNode head1, SinglyLinkedListNode head2) {
+	// 	if(head1 == null) {
+	// 		return head2;
+	// 	}
+	// 	if(head2 == null) {
+	// 		return head1;
+	// 	}
+
+	// 	if(head1.data < head2.data) {
+	// 		head1.next = mergeLists(head1.next, head2);
+	// 		return head1;
+	// 	} else {
+	// 		head2.next = mergeLists(head1, head2.next);
+	// 		return head2;
+	// 	}
+	// }
 }
 
 type stack struct {
